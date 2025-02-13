63b77abc4eee93ee94d41af8b56ac443daa797df
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-13 11:44:13 +0300

    Use strategies

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import helmet from "helmet";
+import passport from "passport";
+import { jwtStrategy, localStrategy } from "./lib/passport.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+passport.use(jwtStrategy);
+passport.use(localStrategy);
 
 const PORT = process.env.PORT || 3300;
 
