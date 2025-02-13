e778936ca422b0a2f7f31eaad81d4b337c62ee9e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-13 12:53:22 +0300

    Handle errors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import passport from "passport";
 import { jwtStrategy, localStrategy } from "./lib/passport.js";
 import routes from "./routes/index.js";
+import createHttpError from "http-errors";
 
 const app = express();
 
@@ -14,6 +15,18 @@ passport.use(jwtStrategy);
 passport.use(localStrategy);
 
 app.use(routes);
+
+app.use((req, res, next) => next(createHttpError(404)));
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+    const code = err.statusCode || 500;
+
+    if (code >= 500) {
+        console.error(err);
+    }
+
+    res.sendStatus(code);
+});
 
 const PORT = process.env.PORT || 3300;
 
