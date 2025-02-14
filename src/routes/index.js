f19a368b488aa524ba66b724efeff68577e08c27
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-14 20:01:58 +0300

    Add posts router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import signup from "./signup/index.js";
 import login from "./login/index.js";
+import posts from "./posts/index.js";
 
-export default [signup, login];
+export default [signup, login, posts];
