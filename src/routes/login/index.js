5596b88426f01b0eb425239113ca6b5846c16987
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-15 09:06:12 +0300

    Refactor

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import login from "../../controllers/login.js";
+import passport from "passport";
 
 const router = Router();
 
-router.post("/log-in", login.post);
+router.post(
+    "/log-in",
+    passport.authenticate("local", { session: false }),
+    login.post,
+);
 
 export default router;
