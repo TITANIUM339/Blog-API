e6943adb73e2a96148aa1261eb549c0d55da7ae1
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-16 12:27:28 +0300

    Create get route

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import posts from "../../controllers/posts.js";
 import passport from "passport";
+import optionalAuth from "../../middleware/optionalAuth.js";
 
 const router = Router();
 
-router.post(
-    "/posts",
-    passport.authenticate("jwt", { session: false }),
-    posts.post,
-);
+router
+    .route("/posts")
+    .get(optionalAuth, posts.get)
+    .post(passport.authenticate("jwt", { session: false }), posts.post);
 
 export default router;
