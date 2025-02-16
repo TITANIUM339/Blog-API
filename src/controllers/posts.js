7b584d45cb81e08caf0bfaae8b44e6d6138a60b7
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-16 12:25:15 +0300

    Get posts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,9 +1,31 @@
 import expressAsyncHandler from "express-async-handler";
-import { validatePost } from "../lib/validation.js";
+import { validatePost, validatePostsQuery } from "../lib/validation.js";
 import { matchedData, validationResult } from "express-validator";
 import prisma from "../lib/prisma.js";
 
 export default {
+    get: [
+        validatePostsQuery(),
+        expressAsyncHandler(async (req, res) => {
+            const result = validationResult(req);
+
+            if (!result.isEmpty()) {
+                res.status(400).json({ errors: result.array() });
+
+                return;
+            }
+
+            const { myPosts, published } = matchedData(req);
+
+            const posts = await prisma.post.findMany({
+                where: myPosts
+                    ? { authorId: req.user.id, published }
+                    : { published: true },
+            });
+
+            res.json({ posts });
+        }),
+    ],
     post: [
         validatePost(),
         expressAsyncHandler(async (req, res) => {
