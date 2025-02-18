b1d4f0c77365af2a435ccee444fbdd0dbaf6c812
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-18 12:17:58 +0300

    Add post router

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import post from "./post/index.js";
 import posts from "../../controllers/posts.js";
 import passport from "passport";
 import optionalAuth from "../../middleware/optionalAuth.js";
 
 const router = Router();
 
+const route = "/posts";
+
+router.use(route, post);
+
 router
-    .route("/posts")
+    .route(route)
     .get(optionalAuth, posts.get)
     .post(passport.authenticate("jwt", { session: false }), posts.post);
 
