b1db9f4add549006c90456748e2f61c6bf114a59
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-18 12:07:18 +0300

    Send 404 error on validation fail

---
+++ src/middleware/handleValidationFail.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+export default (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        next("router");
+
+        return;
+    }
+
+    next();
+};
