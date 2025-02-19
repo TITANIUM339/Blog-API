9f09960871442334d7ecd4bad1428d90f38727ee
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-02-19 20:49:16 +0300

    Update post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,6 +1,8 @@
 import expressAsyncHandler from "express-async-handler";
 import { matchedData } from "express-validator";
 import prisma from "../lib/prisma.js";
+import { validatePostUpdate } from "../lib/validation.js";
+import { validationResult } from "express-validator";
 
 export default {
     get: expressAsyncHandler(async (req, res) => {
@@ -10,4 +12,22 @@ export default {
 
         res.json({ post });
     }),
+    put: [
+        validatePostUpdate(),
+        expressAsyncHandler(async (req, res) => {
+            const result = validationResult(req);
+
+            if (!result.isEmpty()) {
+                res.status(400).json({ errors: result.array() });
+
+                return;
+            }
+
+            const { postId, ...data } = matchedData(req);
+
+            await prisma.post.update({ where: { id: postId }, data });
+
+            res.sendStatus(200);
+        }),
+    ],
 };
