c0f1625f9089b9850e038a5a0b3d1305d8329a5a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-05-07 17:03:19 +0300

    Add get route

--- src/routes/posts/post/comments/index.js
+++ src/routes/posts/post/comments/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import comments from "../../../../controllers/comments.js";
 import validatePostAccess from "../../../../middleware/validatePostAccess.js";
+import optionalAuth from "../../../../middleware/optionalAuth.js";
 import passport from "passport";
 
 const router = Router();
@@ -13,6 +14,7 @@ router
         passport.authenticate("jwt", { session: false }),
         validatePostAccess,
         comments.post,
-    );
+    )
+    .get(optionalAuth, validatePostAccess, comments.get);
 
 export default router;
