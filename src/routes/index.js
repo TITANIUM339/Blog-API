9ab7ef98527bdc43cf39e38b393fa3c337d9dc72
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-02 20:55:53 +0300

    Add user router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import signup from "./signup/index.js";
 import login from "./login/index.js";
 import posts from "./posts/index.js";
+import user from "./user/index.js";
 
-export default [signup, login, posts];
+export default [signup, login, posts, user];
