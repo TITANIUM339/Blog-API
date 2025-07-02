fd0181c1e6cd6bc13a089b6446a932896ee27999
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-07-02 14:52:00 +0300

    Include user id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,7 @@
 export default {
     get(req, res) {
         // eslint-disable-next-line no-unused-vars
-        const { id, passwordHash, ...user } = req.user;
+        const { passwordHash, ...user } = req.user;
 
         res.json(user);
     },
